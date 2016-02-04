bd6f54520fef94236f6a1f0cd34193801797ad92
Colin <colinmatthew@gmail.com>
2016-02-04 13:57:04 -0500

    adding physics stuff

--- src/Classifier.h
+++ src/Classifier.h
@@ -20,39 +20,46 @@ using namespace caffe;
 using std::string;
 
 /* Pair (label, confidence) representing a prediction. */
-typedef std::pair<string, float> Prediction;
+typedef std::vector<float> Prediction;
 
 class Classifier {
     public:
         Classifier(const string& model_file,
                    const string& trained_file,
                    const string& mean_file,
-                   const string& label_file);
+                   const string& min_file,
+                   const string& max_file);
 
-        std::vector<Prediction> Classify(const cv::Mat& img, int N = 5);
+        Prediction Classify(std::vector<float> input);
 
     private:
         void SetMean(const string& mean_file);
 
-        std::vector<float> Predict(const cv::Mat& img);
+        void SetMin(const string& mean_file);
+        
+        void SetMax(const string& mean_file);
 
-        void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+        std::vector<float> Predict(std::vector<float>);
 
-        void Preprocess(const cv::Mat& img,
-                        std::vector<cv::Mat>* input_channels);
+        // void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+
+        void Preprocess(std::vector<float> &data);
 
     private:
         shared_ptr<Net<float> > net_;
         cv::Size input_geometry_;
         int num_channels_;
-        cv::Mat mean_;
-        std::vector<string> labels_;
+        std::vector<float> mean_;
+        std::vector<float> min_;
+        std::vector<float> max_;
+        // std::vector<string> labels_;
 };
 
 Classifier::Classifier(const string& model_file,
                        const string& trained_file,
                        const string& mean_file,
-                       const string& label_file) {
+                       const string& min_file,
+                       const string& max_file) {
 #ifdef CPU_ONLY
     Caffe::set_mode(Caffe::CPU);
 #else
@@ -60,98 +67,134 @@ Classifier::Classifier(const string& model_file,
 #endif
 
     /* Load the network. */
+    std::cout << std::endl << "got initialized" << std::endl;
     net_.reset(new Net<float>(model_file, TEST));
+    std::cout << std::endl << "got loaded" << std::endl;
     net_->CopyTrainedLayersFrom(trained_file);
-
+    std::cout << std::endl << "got weighted" << std::endl;
     Blob<float>* input_layer = net_->input_blobs()[0];
+    std::cout << std::endl << "got fetched blob" << std::endl;
     num_channels_ = input_layer->channels();
-
+    std::cout << std::endl << "got channels: " << num_channels_ << std::endl;
     input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
-
+    std::cout << std::endl << "got dims: "
+            << input_layer->width() << "," << input_layer->height() << std::endl;
     /* Load the binaryproto mean file. */
     SetMean(mean_file);
-
-    /* Load labels. */
-    std::ifstream labels(label_file.c_str());
-    string line;
-    while (std::getline(labels, line))
-        labels_.push_back(string(line));
-
+    SetMin(min_file);
+    SetMax(max_file);
+    std::cout << std::endl << "got meanminmax" << std::endl;
     Blob<float>* output_layer = net_->output_blobs()[0];
+    std::cout << std::endl << "got outputs" << std::endl;
 }
 
-static bool PairCompare(const std::pair<float, int>& lhs,
-                        const std::pair<float, int>& rhs) {
-    return lhs.first > rhs.first;
-}
+// static bool PairCompare(const std::pair<float, int>& lhs,
+//                         const std::pair<float, int>& rhs) {
+//     return lhs.first > rhs.first;
+// }
 
 /* Return the indices of the top N values of vector v. */
-static std::vector<int> Argmax(const std::vector<float>& v, int N) {
-    std::vector<std::pair<float, int> > pairs;
-    for (size_t i = 0; i < v.size(); ++i)
-        pairs.push_back(std::make_pair(v[i], i));
-    std::partial_sort(pairs.begin(), pairs.begin() + N, pairs.end(), PairCompare);
+// static std::vector<int> Argmax(const std::vector<float>& v, int N) {
+//     std::vector<std::pair<float, int> > pairs;
+//     for (size_t i = 0; i < v.size(); ++i)
+//         pairs.push_back(std::make_pair(v[i], i));
+//     std::partial_sort(pairs.begin(), pairs.begin() + N, pairs.end(), PairCompare);
 
-    std::vector<int> result;
-    for (int i = 0; i < N; ++i)
-        result.push_back(pairs[i].second);
-    return result;
-}
+//     std::vector<int> result;
+//     for (int i = 0; i < N; ++i)
+//         result.push_back(pairs[i].second);
+//     return result;
+// }
 
 /* Return the top N predictions. */
-std::vector<Prediction> Classifier::Classify(const cv::Mat& img, int N) {
-    std::vector<float> output = Predict(img);
-
-    std::vector<int> maxN = Argmax(output, N);
-    std::vector<Prediction> predictions;
-    for (int i = 0; i < N; ++i) {
-        int idx = maxN[i];
-        predictions.push_back(std::make_pair(labels_[idx], output[idx]));
-    }
-
-    return predictions;
+Prediction Classifier::Classify(std::vector<float> input) {
+    Prediction output = Predict(input);
+    
+    return output;
 }
 
 /* Load the mean file in binaryproto format. */
 void Classifier::SetMean(const string& mean_file) {
     BlobProto blob_proto;
     ReadProtoFromBinaryFileOrDie(mean_file.c_str(), &blob_proto);
-
-    /* Convert from BlobProto to Blob<float> */
+    std::cout << "data: " << blob_proto.data(0) << std::endl;
+    std::cout << "data: " << blob_proto.data(1) << std::endl;
+    std::cout << "data: " << blob_proto.data(2) << std::endl;
+    std::cout << "data: " << blob_proto.data(3) << std::endl;
+    std::cout << "data: " << blob_proto.data(4) << std::endl;
+    std::cout << "data: " << blob_proto.data(5) << std::endl;
+    std::cout << "data: " << blob_proto.data(6) << std::endl;
+    std::cout << "data: " << blob_proto.data(7) << std::endl;
+    
     Blob<float> mean_blob;
+    std::cout << "yep";
     mean_blob.FromProto(blob_proto);
-
-    /* The format of the mean file is planar 32-bit float BGR or grayscale. */
-    std::vector<cv::Mat> channels;
+    std::cout << "bye";
     float* data = mean_blob.mutable_cpu_data();
-    for (int i = 0; i < num_channels_; ++i) {
-        /* Extract an individual channel. */
-        cv::Mat channel(mean_blob.height(), mean_blob.width(), CV_32FC1, data);
-        channels.push_back(channel);
-        data += mean_blob.height() * mean_blob.width();
+    for (int i = 0; i < mean_blob.channels(); i++) {
+        mean_.push_back(*data);
+        data += 1;
+        std::cout << "something";
     }
-
-    /* Merge the separate channels into a single image. */
-    cv::Mat mean;
-    cv::merge(channels, mean);
-
-    /* Compute the global mean pixel value and create a mean image
-     * filled with this value. */
-    cv::Scalar channel_mean = cv::mean(mean);
-    mean_ = cv::Mat(input_geometry_, mean.type(), channel_mean);
+    std::cout << "something";
 }
 
-std::vector<float> Classifier::Predict(const cv::Mat& img) {
+void Classifier::SetMin(const string& min_file) {
+    BlobProto blob_proto;
+    ReadProtoFromBinaryFileOrDie(min_file.c_str(), &blob_proto);
+
+    /* Convert from BlobProto to Blob<float> */
+    Blob<float> min_blob;
+
+    min_blob.FromProto(blob_proto);
+
+    float* data = min_blob.mutable_cpu_data();
+    for (int i = 0; i < min_blob.channels(); i++) {
+        min_.push_back(*data);
+        data += 1;
+    }
+}
+
+void Classifier::SetMax(const string& max_file) {
+    BlobProto blob_proto;
+    ReadProtoFromBinaryFileOrDie(max_file.c_str(), &blob_proto);
+
+    /* Convert from BlobProto to Blob<float> */
+    Blob<float> max_blob;
+    max_blob.FromProto(blob_proto);
+
+    float* data = max_blob.mutable_cpu_data();
+    for (int i = 0; i < max_blob.channels(); i++) {
+        max_.push_back(*data);
+        data += 1;
+    }
+}
+
+/*
+This will be the FORWARD PASS operation on input data
+*/
+std::vector<float> Classifier::Predict(std::vector<float> input) {
+    std::cout << std::endl << "got here" << std::endl;
     Blob<float>* input_layer = net_->input_blobs()[0];
+    std::cout << std::endl << "and here" << std::endl;
     input_layer->Reshape(1, num_channels_,
                          input_geometry_.height, input_geometry_.width);
     /* Forward dimension change to all layers. */
     net_->Reshape();
 
-    std::vector<cv::Mat> input_channels;
-    WrapInputLayer(&input_channels);
+    Preprocess(input);
 
-    Preprocess(img, &input_channels);
+    // @DEBUG: this probably won't work
+    std::cout << "Input Num: " << input_layer->num()
+            << "Input Channels: " << input_layer->channels()
+            << "Input Width: " << input_layer->width()
+            << "Input Height: " << input_layer->height()
+            << std::endl;
+    float* input_data = input_layer->mutable_cpu_data();
+    for (int i = 0; i < input_layer->channels(); i++) {
+        *input_data = input.at(i);
+        input_data += (input_layer->width() * input_layer->height());
+    }
 
     net_->ForwardPrefilled();
 
@@ -162,58 +205,18 @@ std::vector<float> Classifier::Predict(const cv::Mat& img) {
     return std::vector<float>(begin, end);
 }
 
-/* Wrap the input layer of the network in separate cv::Mat objects
- * (one per channel). This way we save one memcpy operation and we
- * don't need to rely on cudaMemcpy2D. The last preprocessing
- * operation will write the separate channels directly to the input
- * layer. */
-void Classifier::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
-    Blob<float>* input_layer = net_->input_blobs()[0];
-
-    int width = input_layer->width();
-    int height = input_layer->height();
-    float* input_data = input_layer->mutable_cpu_data();
-    for (int i = 0; i < input_layer->channels(); ++i) {
-        cv::Mat channel(height, width, CV_32FC1, input_data);
-        input_channels->push_back(channel);
-        input_data += width * height;
+/*
+This will be where we normalize the data prior to the forward pass
+*/
+void Classifier::Preprocess(std::vector<float> &data) {
+    // Normalize the data
+    int desired_min = -1;
+    int desired_max = 1;
+    for (int i = 0; i < data.size(); i++) {
+        data[i] = data[i] - mean_[i];
+        data[i] = (((data[i] - min_[i]) * (desired_max - desired_min))/(max_[i] - min_[i])) 
+            + desired_min;
     }
-}
-
-void Classifier::Preprocess(const cv::Mat& img,
-                            std::vector<cv::Mat>* input_channels) {
-    /* Convert the input image to the input image format of the network. */
-    cv::Mat sample;
-    if (img.channels() == 3 && num_channels_ == 1)
-        cv::cvtColor(img, sample, CV_BGR2GRAY);
-    else if (img.channels() == 4 && num_channels_ == 1)
-        cv::cvtColor(img, sample, CV_BGRA2GRAY);
-    else if (img.channels() == 4 && num_channels_ == 3)
-        cv::cvtColor(img, sample, CV_BGRA2BGR);
-    else if (img.channels() == 1 && num_channels_ == 3)
-        cv::cvtColor(img, sample, CV_GRAY2BGR);
-    else
-        sample = img;
-
-    cv::Mat sample_resized;
-    if (sample.size() != input_geometry_)
-        cv::resize(sample, sample_resized, input_geometry_);
-    else
-        sample_resized = sample;
-
-    cv::Mat sample_float;
-    if (num_channels_ == 3)
-        sample_resized.convertTo(sample_float, CV_32FC3);
-    else
-        sample_resized.convertTo(sample_float, CV_32FC1);
-
-    cv::Mat sample_normalized;
-    cv::subtract(sample_float, mean_, sample_normalized);
-
-    /* This operation will write the separate BGR planes directly to the
-     * input layer of the network because it is wrapped by the cv::Mat
-     * objects in input_channels. */
-    cv::split(sample_normalized, *input_channels);
 }
 
 #endif
---
+++ src/forward_physics_toy_car.cpp
@@ -0,0 +1,86 @@
+/*
+ * ros_caffe_test.cpp
+ *
+ *  Created on: Aug 31, 2015
+ *      Author: Tzutalin
+ */
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <std_msgs/String.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include "Classifier.h"
+#include <std_msgs/Float32MultiArray.h>
+
+const std::string RECEIVE_IMG_TOPIC_NAME = "/toy_car_input";
+const std::string PUBLISH_RET_TOPIC_NAME = "/toy_car_output";
+
+Classifier* classifier;
+std::string model_path;
+std::string weights_path;
+std::string mean_file;
+std::string min_file;
+std::string max_file;
+
+ros::Publisher gPublisher;
+
+void publishRet(Prediction& prediction);
+
+void inputCallback(const std_msgs::Float32MultiArray::ConstPtr& input) {
+    float data[8];
+    for (int i = 0; i < (sizeof(data) / sizeof(data[0])); i++) {
+        data[i] = input->data[i];
+    }
+
+    std::vector<float> input_data(data, data + (sizeof(data) / sizeof(data[0])));
+
+    Prediction prediction = classifier->Classify(input_data);
+	
+    publishRet(prediction);
+}
+
+// TODO: Define a msg or create a service
+// Try to receive : $rostopic echo /caffe_ret
+void publishRet(Prediction& prediction)  {
+    std_msgs::Float32MultiArray msg;
+    for (int i = 0; i < prediction.size(); i++) {
+      msg.data.push_back(prediction[i]);
+    }
+    gPublisher.publish(msg);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "ros_caffe_test");
+    ros::NodeHandle nh;
+    // To receive an image from the topic, PUBLISH_RET_TOPIC_NAME
+    ros::Subscriber sub = nh.subscribe(RECEIVE_IMG_TOPIC_NAME, 1, inputCallback);
+	gPublisher = nh.advertise<std_msgs::String>(PUBLISH_RET_TOPIC_NAME, 100);
+    const std::string ROOT_SAMPLE = ros::package::getPath("ros_caffe");
+    model_path = ROOT_SAMPLE + "/caffe/examples/toy_car/PhysicsLearner/toycar_2fc_deploy.prototxt";
+    weights_path = ROOT_SAMPLE + "/caffe/examples/toy_car/PhysicsLearner/2fc_iter_20001.caffemodel";
+    mean_file = ROOT_SAMPLE + "/caffe/examples/toy_car/PhysicsLearner/toy_car_mean.binaryproto";
+    min_file = ROOT_SAMPLE + "/caffe/examples/toy_car/PhysicsLearner/toy_car_min.binaryproto";
+    max_file = ROOT_SAMPLE + "/caffe/examples/toy_car/PhysicsLearner/toy_car_max.binaryproto";
+    // label_file = ROOT_SAMPLE + "/data/synset_words.txt";
+    // image_path = ROOT_SAMPLE + "/data/cat.jpg";
+
+    classifier = new Classifier(model_path, weights_path, mean_file, min_file, max_file);
+
+    // Test data/cat.jpg
+    // cv::Mat img = cv::imread(image_path, -1);
+
+ //    std::vector<Prediction> predictions = classifier->Classify(img);
+ //    /* Print the top N predictions. */
+ //    std::cout << "Test default image under /data/cat.jpg" << std::endl;
+ //    for (size_t i = 0; i < predictions.size(); ++i) {
+ //        Prediction p = predictions[i];
+ //        std::cout << std::fixed << std::setprecision(4) << p.second << " - \"" << p.first << "\"" << std::endl;
+ //    }
+	// publishRet(predictions);
+
+    ros::spin();
+    delete classifier;
+    ros::shutdown();
+    return 0;
+}
